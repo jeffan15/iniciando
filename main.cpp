8f397cc536bf814fec9b5618bdceef52c5381b7a
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2018-08-22 09:49:51 -0500

    hi

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=21474836;
+    int b;
+    cin>>b;
+    int z=a+b;
+    int r=a/b;
+    cout<<"a es"<<a<<endl;
+    cout<<"b es"<<b<<endl;
+    cout<<"la suma es"<<z<<endl;
+    cout<<"la multiplicación es"<<a*b<<endl;
+    cout<<"la division es"<<r<<endl;
+    return 0;
+}
