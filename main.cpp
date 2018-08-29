433ff417a5d63980bed92a043299342e2535b017
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2018-08-29 09:54:35 -0500

    kaka

--- main.cpp
+++ main.cpp
@@ -2,17 +2,68 @@
 
 using namespace std;
 
-int main()
-{
-    int a=21474836;
-    int b;
-    cin>>b;
-    int z=a+b;
-    int r=a/b;
-    cout<<"a es"<<a<<endl;
-    cout<<"b es"<<b<<endl;
-    cout<<"la suma es"<<z<<endl;
-    cout<<"la multiplicación es"<<a*b<<endl;
-    cout<<"la division es"<<r<<endl;
-    return 0;
+int main{
+    int a = 1;
+    cout<<"numero"<<"\t"<<"potencia 2"<<"\t"<<"potencia 3"<<endl;
+    cout<<"a"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+1"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+2"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+3"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+4"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+5"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+6"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+7"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+8"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    cout<<"a+9"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+    //int a;
+    //cout<<"da in numero de 5 digitos"<<a<<endl;
+    //cin>>a;
+    //int b,c,d,e,f;
+    //if(a>=10000){
+     //   if(a<=100000){
+      //      b=a/10000;
+      //      c=(a%10000)/1000;
+      //      d=((a%10000)%1000)/100;
+     //      e=(((a%10000)%1000)%100)/10;
+     //       f=(((a%10000)%1000)%100)%10;
+      //      cout<<b<<"\t"<<c<<"\t"<<d<<"\t"<<e<<"\t"<<f<<endl;
+     //   }
+    //}
+    //int a,b,c;
+    //cout<<"ingresa a:"<<endl;
+    //cin>>a;
+    //cout<<"ingresa b:"<<endl;
+    //cin>>b;
+    //cout<<"ingresa c:"<<endl;
+    //cin>>c;
+
+    //cout<<"la suma es:"<<a+b+c<<endl;
+    //cout<<"el producto es:"<<a*b*c<<endl;
+    //cout<<"el promedio es:"<<(a+b+c)/3<<endl;
+
+    //if(a>b){
+        //if(a>c){
+            //cout<<"a es el mayor"<<endl;
+       // }
+        //else{
+          //  cout<<"c es el mayor"<<endl;
+        //}
+    //}
+    //else{
+     //   cout<<"b es el mayor"<<endl;
+    //}
+    //if(a%2==0){
+    //    cout<<"a es par"<<endl;
+    //}
+    //else{
+    //    cout<<"a es impar"<<endl;
+    //}
+    //if(a%b==0){
+    //    cout<<"a es multiplo de b"<<endl;
+    //}
+    //else{
+    //    cout<<"no son multiplos"<<endl;
+    //}cout<<"a"<<"\t"<<a*a<<"\t"<<a*a*a<<endl;
+
+//}
 }
